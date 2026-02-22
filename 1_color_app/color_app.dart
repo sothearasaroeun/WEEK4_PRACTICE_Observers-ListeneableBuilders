a1ba737c836a4e921d5107a0e923b368485444ff
sothearasaroeun <sotheara.saroeun.911@gmail.com>
2026-02-22 13:19:53 +0700

    TASK-001: The Color App

--- 1_color_app/color_app.dart
+++ 1_color_app/color_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(body: Home()),
     ),
@@ -10,6 +10,26 @@ void main() {
 }
 
 enum CardType { red, blue }
+
+class ColorService extends ChangeNotifier {
+  int redTapCount = 0;
+  int blueTapCount = 0;
+
+  int get redTapCountGetter => redTapCount;
+  int get blueTapCountGetter => blueTapCount;
+
+  void incrementRed() {
+    redTapCount++;
+    notifyListeners();
+  }
+
+  void incrementBlue() {
+    blueTapCount++;
+    notifyListeners();
+  }
+}
+
+final colorService = ColorService();
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -20,36 +40,13 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
-  int redTapCount = 0;
-  int blueTapCount = 0;
-
-  void _incrementRedTapCount() {
-    setState(() {
-      redTapCount++;
-    });
-  }
-
-  void _incrementBlueTapCount() {
-    setState(() {
-      blueTapCount++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _currentIndex == 0
-              ? ColorTapsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-                onRedTap: _incrementRedTapCount,
-                onBlueTap: _incrementBlueTapCount,
-              )
-              : StatisticsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-              ),
+      body: _currentIndex == 0
+          ? const ColorTapsScreen()
+          : const StatisticsScreen(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -57,7 +54,7 @@ class _HomeState extends State<Home> {
             _currentIndex = index;
           });
         },
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.tap_and_play),
             label: 'Taps',
@@ -73,31 +70,30 @@ class _HomeState extends State<Home> {
 }
 
 class ColorTapsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-  final VoidCallback onRedTap;
-  final VoidCallback onBlueTap;
+  const ColorTapsScreen({super.key});
 
-  const ColorTapsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-    required this.onRedTap,
-    required this.onBlueTap,
-  });
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Color Taps')),
-      body: Column(
-        children: [
-          ColorTap(type: CardType.red, tapCount: redTapCount, onTap: onRedTap),
-          ColorTap(
-            type: CardType.blue,
-            tapCount: blueTapCount,
-            onTap: onBlueTap,
-          ),
-        ],
+      appBar: AppBar(title: const Text('Color Taps')),
+      body: ListenableBuilder(
+        listenable: colorService,
+        builder: (context, child) {
+          return Column(
+            children: [
+              ColorTap(
+                type: CardType.red,
+                tapCount: colorService.redTapCountGetter,
+                onTap: colorService.incrementRed,
+              ),
+              ColorTap(
+                type: CardType.blue,
+                tapCount: colorService.blueTapCountGetter,
+                onTap: colorService.incrementBlue,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
@@ -122,7 +118,7 @@ class ColorTap extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
         decoration: BoxDecoration(
           color: backgroundColor,
           borderRadius: BorderRadius.circular(10),
@@ -132,7 +128,7 @@ class ColorTap extends StatelessWidget {
         child: Center(
           child: Text(
             'Taps: $tapCount',
-            style: TextStyle(fontSize: 24, color: Colors.white),
+            style: const TextStyle(fontSize: 24, color: Colors.white),
           ),
         ),
       ),
@@ -141,27 +137,31 @@ class ColorTap extends StatelessWidget {
 }
 
 class StatisticsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-
-  const StatisticsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-  });
+  const StatisticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Statistics')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Red Taps: $redTapCount', style: TextStyle(fontSize: 24)),
-            Text('Blue Taps: $blueTapCount', style: TextStyle(fontSize: 24)),
-          ],
-        ),
+      appBar: AppBar(title: const Text('Statistics')),
+      body: ListenableBuilder(
+        listenable: colorService,
+        builder: (context, child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Red Taps: ${colorService.redTapCountGetter}',
+                  style: const TextStyle(fontSize: 24),
+                ),
+                Text(
+                  'Blue Taps: ${colorService.blueTapCountGetter}',
+                  style: const TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
